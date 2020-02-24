4ef09749433b2324b85b1cdc81e27e730d675a56
Harun Gunes <h.gunes13@gmail.com>
2020-02-24 22:57:57 +0100

    Initial Commit

---
+++ Color Palette Generator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Color Palette Generator
+//
+//  Created by Harun Gunes on 24/02/2020.
+//  Copyright © 2020 Harun Gunes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
